10f42f9f2722501e0078025add85d6a4e6040e02
User <gtzquierdo@protonmail.com>
2022-08-21 14:50:04 -0400

    first commit w/ prep exercise chapt1 complete

---
+++ foobar.js
@@ -0,0 +1,27 @@
+let foo = "bar";
+console.log(foo);
+foo;
+
+/* Identify the Constructors for each of the following methods and classify each method as either a "Static" or a "Prototype" method:
+
+substring:String - Prototype
+create: Object - Static
+fromCharCode: String - Static
+slice: Array, String - Prototype
+toString: String, object, array, number - Prototype
+
+Which of the following names satisfy the style guidelines for variable names? For the purposes of this question, constants are not variables.
+
+- index - lazyDog - operand2
+
+Which of the following names satisfy the style guidelines for function names?
+CatName - index - lazyDog - operand2
+
+Which of the following names satisfy the style guidelines for constants used to represent magic numbers?
+
+BIG_NUMBER
+
+Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
+
+snake_case - quick_Fox - 1stCharacter
+*/
